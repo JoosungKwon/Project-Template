6b61be6534e5e6453cf1bc769643ca57dec35d6a
kwonjoosung <kwonjoosung@gmail.com>
2023-02-15 19:03:56 +0900

    Exception 템플릿 수정
    - MessageSource를 활용해서 메시지를 관리할 수 있도록 변경

--- src/main/java/com/prgrms/demotemplate/global/exception/ServiceRuntimeException.java
+++ src/main/java/com/prgrms/demotemplate/global/exception/ServiceRuntimeException.java
@@ -1,26 +1,19 @@
 package com.prgrms.demotemplate.global.exception;
 
-public class ServiceRuntimeException extends RuntimeException {
+import lombok.Getter;
 
-	protected ServiceRuntimeException() {
+@Getter
+public abstract class ServiceRuntimeException extends RuntimeException {
+
+	protected static final String DOT = ".";
+
+	private final String messageKey;
+
+	private final Object[] params;
+
+	protected ServiceRuntimeException(String messageKey, Object[] params) {
+		this.messageKey = messageKey;
+		this.params = params;
 	}
-
-	public ServiceRuntimeException(String message) {
-		super(message);
-	}
-
-	public ServiceRuntimeException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	public ServiceRuntimeException(Throwable cause) {
-		super(cause);
-	}
-
-	public ServiceRuntimeException(String message, Throwable cause, boolean enableSuppression,
-		boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
 }
 
