e94952fb4f44df5d01349338323045a2386679fd
kwonjoosung <kwonjoosung@gmail.com>
2023-02-15 18:11:44 +0900

    JPA Auditing 기능

---
+++ src/main/java/com/prgrms/demotemplate/global/config/JpaConfig.java
@@ -0,0 +1,28 @@
+package com.prgrms.demotemplate.global.config;
+
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.prgrms.demotemplate.global.security.jwt.JwtAuthentication;
+
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareProvider")
+@Configuration
+public class JpaConfig {
+
+	@Bean
+	public AuditorAware<String> auditorAwareProvider() {
+		return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+			.map(SecurityContext::getAuthentication)
+			.filter(Authentication::isAuthenticated)
+			.map(authentication -> (JwtAuthentication)authentication.getPrincipal())
+			.map(user -> user.id().toString());
+	}
+}
+
